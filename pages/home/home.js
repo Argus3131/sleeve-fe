c850f304c362adca9bcd52dbbb1e892d01545a32
Argus <1043521503@qq.com>
2020-02-19 00:40:29 +0800

    # 新增: 优化模型类Theme 以对象保存数据和状态 取消一些类变量和类方法 数据处理放在内部 减少硬编码

--- pages/home/home.js
+++ pages/home/home.js
@@ -55,7 +55,16 @@ Page({
     const bannerB = await Banner.getLocationB()
     const gridC = await Category.getLocationC()
     const activityD = await Activity.getLocationD()
-    const scollersE = await Theme.getLocationE_withSpu('t-2')
+    // 我们将硬编码的数据't-2' 放在类内部处理掉 await Theme.getLocationESpu('t-2')
+    // 由于scollersE对应的是每日上新 因此我们可以加个判断是否上下架子
+    let scollersE = null
+    let scollE_spu_List
+    if (themeA.online) {
+      scollersE = await Theme.getLocationESpu()
+      if (scollersE) {
+        scollE_spu_List = scollersE["spu_list"].slice(0,7) //截取长度为8
+      }
+    }
     const themeF = theme.getLocationF()
     const bannerG = await Banner.getLocationG()
     const selling_arr = bannerG.items
@@ -76,6 +85,7 @@ Page({
       gridC: gridC,
       activityD: activityD,
       scollersE: scollersE,
+      scollE_spu_List:scollE_spu_List,
       themeF: themeF,
       bannerG: bannerG,
       themeH: themeH,
@@ -120,10 +130,11 @@ Page({
   },
   async onReachBottom (event) {
     let start_num
-    // 判断当前请求情况
+    // 判断当前请求情况 假如是正常的状态 每次就 增5
     if (this.data.requestStatus_skuLate) {
       start_num = this.data.start_num += 5
     } else {
+      // 否则让客户端发送重复的请求
       start_num = this.data.start_num
     }
     // 加锁 防止多次重复触底发送请求 等待一个请求完成后 渲染完成后再次允许发送请求
@@ -136,11 +147,17 @@ Page({
       start_num: start_num,
 
     })
+    // 因为promise的reject 报错 是不往外抛的 我们用catch去获取错误信息
     const skuLatest_res = await WaterFlow.getSkuLatest(start_num).catch((reason) => {
       if (reason.errMsg) {
+        // 请求失败 更改请求状态
         this.setData({
           requestStatus_skuLate: false
         })
+        // 解锁 因为假如在失败情况下不解锁 的后果：
+        // 会造成死锁 因为程序卡在了这个异常地方不走下去了就会一直处于locked
+        // 解决方案：在错误状态下的catch代码块里是能执行的 在里面去开锁即可
+        console.log("test open lock")
         this._unlocked()
       }
     })
@@ -156,7 +173,8 @@ Page({
     //判断当前的sku_arr是否为空数组
     // 1）特殊场景断网 传递是[] 导致死锁 的处理 这边使用对状态和 断网情况错误信息判断对锁的状态同步进行改变
     const flag = this._hasMore(_items_new)
-    // items_new数组是否为空
+    // items_new数组是否为空来判断是否发送了一次完整的http请求
+    // 是在网络良好 成功发送请求 响应能获取数组 这个逻辑下进行的 所以上面需要对网络异常的情况进行处理
     if (flag) {
       this._unlocked()
       const items_new = this.processData_SkuLatest(_items_new)
